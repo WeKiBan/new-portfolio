f6c163ffbbaed8c7d69ae05024d0c0aa7c9e4b91
Wekiban <wes.banyard@googlemail.com>
2021-09-27 12:59:58 +0200

    orientation bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
   wrapper: {
     position: 'relative',
     overflow: 'hidden',
+    maxHeight: '-webkit-fill-available',
     width: '100%',
     height: '100vh',
     display: 'flex',
