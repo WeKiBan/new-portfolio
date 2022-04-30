35afd823efdfc3ea25a0474678a17ad18308aa29
Wekiban <wes.banyard@googlemail.com>
2022-04-30 18:26:01 +0200

    feat: add new skills typescript and redux

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
-import { Typography } from '@material-ui/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import { Typography } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHtml5,
   faCss3,
@@ -10,49 +10,52 @@ import {
   faBootstrap,
   faSass,
   faReact,
-} from '@fortawesome/free-brands-svg-icons';
-import Grid from '@material-ui/core/Grid';
-import { Icon } from '@iconify/react';
-import nextIcon from '../images/nextIcon.svg';
+} from "@fortawesome/free-brands-svg-icons";
+import Grid from "@material-ui/core/Grid";
+import { Icon } from "@iconify/react";
+import nextIcon from "../images/nextIcon.svg";
+import typescriptIcon from "../images/typescriptIcon.svg";
+import reduxIcon from "../images/reduxIcon.svg";
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    background: '#333333',
+    position: "relative",
+    gap: "10px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#333333",
     margin: 0,
     padding: theme.spacing(2),
-    color: 'orange',
+    color: "orange",
   },
   waveTop: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
-    width: '100%',
+    width: "100%",
   },
   heading: {
     padding: theme.spacing(2),
     fontWeight: 400,
   },
   skills: {
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
     maxWidth: 900,
-    fontSize: '5rem !important',
-    color: 'orange',
+    fontSize: "5rem !important",
+    color: "orange",
     padding: 0,
     margin: 0,
     paddingBottom: theme.spacing(4),
   },
   logo: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   svgIcon: {
-    width: '100px',
+    width: "80px",
+    height: "80px",
   },
 }));
 
@@ -61,7 +64,7 @@ function Skills({ english }) {
   return (
     <Box className={classes.wrapper}>
       <Typography className={classes.heading} variant="h4">
-        {english ? 'Skills' : 'Abilità'}
+        {english ? "Skills" : "Abilità"}
       </Typography>
       <Grid container className={classes.skills}>
         <Grid
@@ -174,8 +177,40 @@ function Skills({ english }) {
           sm={4}
           md={2}
         >
-          <Icon icon="mdi:material-ui" styles={{ color: 'orange' }} />
+          <Icon icon="mdi:material-ui" styles={{ color: "orange" }} />
           <Typography>Material UI</Typography>
+        </Grid>
+        <Grid
+          data-aos="fade-left"
+          data-aos-once="true"
+          data-aos-duration="500"
+          ease-in="true"
+          item
+          className={classes.logo}
+          xs={6}
+          sm={4}
+          md={2}
+        >
+          <img className={classes.svgIcon} src={reduxIcon} alt="Redux Icon" />
+          <Typography>Redux Toolkit</Typography>
+        </Grid>
+        <Grid
+          data-aos="fade-left"
+          data-aos-once="true"
+          data-aos-duration="500"
+          ease-in="true"
+          item
+          className={classes.logo}
+          xs={6}
+          sm={4}
+          md={2}
+        >
+          <img
+            className={classes.svgIcon}
+            src={typescriptIcon}
+            alt="Typescript Icon"
+          />
+          <Typography>TypeScript</Typography>
         </Grid>
       </Grid>
     </Box>
